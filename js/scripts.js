e35e2263db0dfec9a14691d56c4d3cc9303aa3f8
lilbitsnpieces <alreadywrote@gmail.com>
2023-01-30 21:07:47 -0800

    Add in tests and functions

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//Business Logic
+function wordCounter(text) {
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}
+
+function wordCounter(text) {
+  if (text.length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}
