c15f1baf45a8bf1b6c53f9da064b71febb5018d5
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-01 20:07:33 -0800

    Add more tests and functions

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,17 @@ function wordCounter(text) {
   });
   return wordCount;
 }
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
